7ad774cc9bad3263f1e289e337df2f4d4d305bf7
qrs-ljy <1450053947@qq.com>
2022-10-08 15:52:57 +0800

    2022年10月8日第三次提交 地点：bjsasc 提交人：ljy 目标：希望有计划的进行

---
+++ atguiguLearnSpring5/Spring5_demo6/src/main/java/com/atguigu/spring5/test/JTest5.java
@@ -0,0 +1,33 @@
+package com.atguigu.spring5.test;
+
+import com.atguigu.spring5.service.UserService;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+/**
+ * @Author 不要有情绪的  ljy
+ * @Date 2022/10/8 15:03
+ * @Description: 整合JUnit5
+ */
+
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration("classpath:bean1.xml")
+
+@SpringJUnitConfig(locations = "classpath:bean1.xml")
+public class JTest5 {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void test5() {
+        userService.accountMoney();
+    }
+
+}
